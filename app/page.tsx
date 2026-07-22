9b22bf593b52d913d56914fc2be05d7ea176f697
Arslan Maslesa <a.maslesa002@gmail.com>
2026-07-22 19:13:44 +0200

    Website Name Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Desktop from '../components/desktop/Desktop';
 
 export const metadata = {
-  title: "Arslan's Portfolio",
+  title: "Arslan Maslesa",
 };
 
 export default function Page() {
